3be5d983968c5ec623d6f9bb01f8d02bdd192a2a
Luca Ban <mesqueeb@gmail.com>
2020-06-29 09:32:03 +0900

    feat: improve error logging

--- packages/api-card/src/helpers/errors.js
+++ packages/api-card/src/helpers/errors.js
@@ -1,8 +1,9 @@
-export const noRequiredPropExampleErrorMsg = `You need to provide an example for all required props to be able to generate a preview.
+export const noRequiredPropExampleErrorMsg = `You **must** provide an example for all _required_ props to be able to generate an interactive preview!!
 Eg.
 \`\`\`js
 props: {
-  /*
+  /**
+   * @type {string[]}
    * @example ['First Tab', 'Second Tab']
    */
   labels: { type: Array, required: true },
