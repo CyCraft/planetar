a6c3ac009feb3bc5b2c0f6e1c8c62398be185be0
Luca Ban <mesqueeb@gmail.com>
2020-07-10 17:00:26 +0900

    fix: prevent examples from showing in the component gallery

--- packages/component-gallery/src/helpers/listOfComponents.js
+++ packages/component-gallery/src/helpers/listOfComponents.js
@@ -1,4 +1,18 @@
 /**
+ * Returns an etry with the file category and the entry.
+ *
+ * @param {string} filePath
+ * @returns {['atoms' | 'molecules' | 'atomExamples' | 'moleculeExamples', string]}
+ */
+function filePathToCategoryPathEntry(filePath) {
+  const p = filePath.startsWith('./') ? filePath.slice(2) : filePath
+  if (p.startsWith('examples/molecules/') || p.includes('/examples/molecules/')) return ['moleculeExamples', p] // prettier-ignore
+  if (p.startsWith('examples/atoms/') || p.includes('/examples/atoms/')) return ['atomExamples', p]
+  if (p.startsWith('molecules/') || p.includes('/molecules/')) return ['molecules', p]
+  if (p.startsWith('atoms/') || p.includes('/atoms/')) return ['atoms', p]
+}
+
+/**
  * @param {string[]} componentFilesList Example: `['./atoms/MyButton.vue']`
  * @returns {{
     atoms: string[],
@@ -7,21 +21,13 @@
     moleculeExamples: string[],
   }}
  */
-export function getComponentPaths (componentFilesList) {
-  const components = componentFilesList
-    .filter(filePath => !filePath.includes('index.js'))
-    .map(filePath => (filePath.startsWith('./') ? filePath.slice(2) : filePath))
-
-  const atoms = components.filter(filePath => filePath.startsWith('atoms/'))
-  const molecules = components.filter(
-    filePath => filePath.startsWith('molecules/') || filePath.includes('/molecules/')
-  )
-  const atomExamples = components.filter(
-    filePath => filePath.startsWith('examples/atoms/') || filePath.includes('/examples/atoms/')
-  )
-  const moleculeExamples = components.filter(
-    filePath =>
-      filePath.startsWith('examples/molecules/') || filePath.includes('/examples/molecules/')
-  )
-  return { atoms, atomExamples, molecules, moleculeExamples }
+export function getComponentPaths(componentFilesList) {
+  const base = { atoms: [], atomExamples: [], molecules: [], moleculeExamples: [] }
+  return componentFilesList
+    .filter((filePath) => !filePath.includes('index.js'))
+    .reduce((carry, filePath) => {
+      const [category, path] = filePathToCategoryPathEntry(filePath)
+      carry[category].push(path)
+      return carry
+    }, base)
 }
