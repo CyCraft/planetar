06927d4ca142fdbf321a94590e8c61d2d8242426
Luca Ban <mesqueeb@gmail.com>
2020-10-08 14:09:40 +0900

    chore: update dependencies

--- packages/dev/src/pages/markdown/CodeBlockSection  examples.vue
+++ packages/dev/src/pages/markdown/CodeBlockSection  examples.vue
@@ -2,8 +2,8 @@
   <q-page padding class="q-gutter-lg">
     <CodeBlockSection filePath="pages/markdown/testJs.js" />
     <CodeBlockSection filePath="pages/markdown/testHtml.html" />
-    <CodeBlockSection filePath="pages/markdown/TestMarkdown.vue" />
     <CodeBlockSection filePath="pages/markdown/testCss.css" />
+    <CodeBlockSection filePath="pages/markdown/Markdown examples.vue" />
   </q-page>
 </template>
 
