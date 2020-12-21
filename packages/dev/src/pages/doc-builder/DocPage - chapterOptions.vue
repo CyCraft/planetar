49c6e4935a764fc72d5d62d6754fa556774a0699
Luca Ban <mesqueeb@gmail.com>
2020-12-21 20:50:54 +0900

    fix: improve examples

---
+++ packages/dev/src/pages/doc-builder/DocPage - chapterOptions.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <DocPage
+      :chapterOrder="['iframe-example.vue']"
+      :chapterOptions="{ 'iframe-example.vue': { iframe: 'https://blitzar-vuetify.web.app' } }"
+      pathToChapterFiles="examples/"
+      :pathsToApiCardSourceFile="['components/atoms/EfButton.vue']"
+      @TOC="log"
+    />
+  </q-page>
+</template>
+
+<style lang="sass" scoped></style>
+
+<script>
+export default {
+  data() {
+    return { log: console.log }
+  },
+}
+</script>
